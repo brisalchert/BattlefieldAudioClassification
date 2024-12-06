436117f7d73cadb731abaad808cdc04e601c18c5
brisalchert <brisalchert@gmail.com>
2024-12-06 17:49:23 -0500

    Tune model & update weights

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
 import os
+import numpy as np
 from matplotlib import pyplot as plt
 import tensorflow as tf  # Need version 2.10.0 for GPU support on Windows
-import tensorflow_io as tfio  # Need to install through: pip install tensorflow_io==0.27.0g
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, Dense, Flatten, Layer
-from itertools import groupby
-import csv
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropout
 
 # Create data pipeline
-training_data = tf.keras.utils.image_dataset_from_directory('data_spectrogram', batch_size=32, image_size=(515, 71),
-                                                            shuffle='false', labels='inferred', label_mode='categorical', seed=42, validation_split=.2, subset='training')
-validation_data = tf.keras.utils.image_dataset_from_directory('data_spectrogram', batch_size=32, image_size=(515, 71),
-                                                              shuffle='false', labels='inferred', label_mode='categorical', seed=42, validation_split=.2, subset='validation')
+training_data, validation_data = tf.keras.utils.image_dataset_from_directory(
+    'data_spectrogram',
+    batch_size=64,
+    image_size=(515, 71),
+    shuffle=True,
+    labels='inferred',
+    label_mode='categorical',
+    seed=42,
+    validation_split=.2,
+    subset='both'
+)
 
 # Build the model
 model = Sequential()
 model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(515, 71, 3)))
 model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(Conv2D(16, (3, 3), activation='relu'))
-model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(5, activation='softmax'))
 
-model.compile(optimizer='Adam', loss='CategoricalCrossentropy', metrics=[tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
+model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=1e-4),
+    loss='CategoricalCrossentropy',
+    metrics=['accuracy']
+)
 model.summary()
 
 # Create weights for classes
-num_launchers = len(os.listdir(r'data_spectrogram/launchers'))
-num_planes = len(os.listdir(r'data_spectrogram/planes'))
-num_rifles = len(os.listdir(r'data_spectrogram/rifles'))
-num_soldiers = len(os.listdir(r'data_spectrogram/soldiers'))
-num_vehicles = len(os.listdir(r'data_spectrogram/vehicles'))
+y_train = np.concatenate([y for x, y in training_data], axis=0)
+
+num_launchers = np.sum([y.argmax() == 0 for y in y_train])
+num_planes = np.sum([y.argmax() == 1 for y in y_train])
+num_rifles = np.sum([y.argmax() == 2 for y in y_train])
+num_soldiers = np.sum([y.argmax() == 3 for y in y_train])
+num_vehicles = np.sum([y.argmax() == 4 for y in y_train])
 num_total = (num_launchers + num_planes + num_rifles + num_soldiers + num_vehicles)
 
 weight_for_launchers = (1 / num_launchers) * (num_total / 2.0)
@@ -44,22 +56,24 @@ weight_for_rifles = (1 / num_rifles) * (num_total / 2.0)
 weight_for_soldiers = (1 / num_soldiers) * (num_total / 2.0)
 weight_for_vehicles = (1 / num_vehicles) * (num_total / 2.0)
 
-class_weight = {0: weight_for_launchers, 1: weight_for_planes, 2: weight_for_rifles, 3: weight_for_soldiers, 4: weight_for_vehicles}
+class_weight = {
+    0: weight_for_launchers,
+    1: weight_for_planes,
+    2: weight_for_rifles,
+    3: weight_for_soldiers,
+    4: weight_for_vehicles
+}
 
 # Fit the model
-hist = model.fit(training_data, epochs=16, validation_data=validation_data, class_weight=class_weight)
+hist = model.fit(training_data, epochs=20, validation_data=validation_data, class_weight=class_weight)
 
 plt.title('Loss')
 plt.plot(hist.history['loss'], 'r')
 plt.plot(hist.history['val_loss'], 'b')
+plt.yscale('log')
 plt.show()
 
-plt.title('Precision')
-plt.plot(hist.history['precision'], 'r')
-plt.plot(hist.history['val_precision'], 'b')
-plt.show()
-
-plt.title('Recall')
-plt.plot(hist.history['recall'], 'r')
-plt.plot(hist.history['val_recall'], 'b')
+plt.title('Accuracy')
+plt.plot(hist.history['accuracy'], 'r')
+plt.plot(hist.history['val_accuracy'], 'b')
 plt.show()
